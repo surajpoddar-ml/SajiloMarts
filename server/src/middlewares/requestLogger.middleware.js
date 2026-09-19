a32711120af6a7c4878ec7740c1fe368a8271317
unknown <surajpoddar098@gmail.com>
2026-09-20 00:40:52 +0545

    Improve application logging

--- server/src/middlewares/requestLogger.middleware.js
+++ server/src/middlewares/requestLogger.middleware.js
@@ -1,6 +1,11 @@
 import morgan from 'morgan';
-import { envConfig } from '../config/index.js';
+import { envConfig, loggerConfig } from '../config/index.js';
 
-export const requestLogger = envConfig.isDevelopment
-  ? morgan('dev')
-  : morgan('combined');
+export const requestLogger = (req, res, next) => {
+  if (loggerConfig.silent) {
+    return next();
+  }
+
+  const format = envConfig.isDevelopment ? 'dev' : 'combined';
+  return morgan(format)(req, res, next);
+};
