205a3fb3698e8f8e626f9624ea633ab581705476
unknown <surajpoddar098@gmail.com>
2026-09-20 02:21:40 +0545

    Establish the account email field

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -6,6 +6,12 @@ const userSchema = new mongoose.Schema({
     required: [true, 'User name is required'],
     trim: true,
   },
+  email: {
+    type: String,
+    required: [true, 'Email address is required'],
+    trim: true,
+    lowercase: true,
+  },
 });
 
 export const User = mongoose.model('User', userSchema);
