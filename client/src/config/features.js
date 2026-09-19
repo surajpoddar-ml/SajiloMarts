a8324867b7bc42b21261882bc388c7b125390401
unknown <surajpoddar098@gmail.com>
2026-09-20 00:06:58 +0545

    Organize client configuration

---
+++ client/src/config/features.js
@@ -0,0 +1,7 @@
+export const FEATURE_FLAGS = {
+  CROSS_BORDER_QUOTES: true,
+  LIVE_TRACKING: true,
+  MULTI_CURRENCY: true,
+  WISHLIST: true,
+  DARK_MODE: true,
+};
--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,2 +1,3 @@
 export * from './env.js';
 export * from './app.js';
+export * from './features.js';
