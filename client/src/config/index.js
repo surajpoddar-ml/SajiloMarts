6843db30618946c7f2c3c3dc7207c383d43fd18d
unknown <surajpoddar098@gmail.com>
2026-09-20 00:08:04 +0545

    Separate public configuration

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,3 +1,4 @@
 export * from './env.js';
 export * from './app.js';
 export * from './features.js';
+export * from './public.js';
---
+++ client/src/config/public.js
@@ -0,0 +1,7 @@
+export const PUBLIC_CONFIG = {
+  BRAND_NAME: 'SastoMarts',
+  TAGLINE: 'India-to-Nepal Cross-Border E-Commerce & Sourcing',
+  SUPPORT_EMAIL: 'support@sastomarts.com',
+  DEFAULT_LOCALE: 'en-NP',
+  SUPPORTED_LOCALES: ['en-NP', 'ne-NP', 'hi-IN', 'en-IN'],
+};
