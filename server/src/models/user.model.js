6f24a2acdfe11fa0bcf957477c01d310e53cbc05
unknown <surajpoddar098@gmail.com>
2026-09-20 02:21:30 +0545

    Configure the customer name field

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,6 +1,12 @@
 import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'User name is required'],
+    trim: true,
+  },
+});
 
 export const User = mongoose.model('User', userSchema);
 export default User;
