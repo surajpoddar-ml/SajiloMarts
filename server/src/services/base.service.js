da61a7485729ea1bc02168bb3a87781778ed2bba
unknown <surajpoddar098@gmail.com>
2026-09-20 00:39:12 +0545

    Improve service structure

--- server/src/services/base.service.js
+++ server/src/services/base.service.js
@@ -1,5 +1,28 @@
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthorizedError,
+  ForbiddenError,
+} from '../utils/index.js';
+
 export class BaseService {
   async execute(action, ...args) {
     return action(...args);
+  }
+
+  throwBadRequest(message) {
+    throw new BadRequestError(message);
+  }
+
+  throwNotFound(message) {
+    throw new NotFoundError(message);
+  }
+
+  throwUnauthorized(message) {
+    throw new UnauthorizedError(message);
+  }
+
+  throwForbidden(message) {
+    throw new ForbiddenError(message);
   }
 }
