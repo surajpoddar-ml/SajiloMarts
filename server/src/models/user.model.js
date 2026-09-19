0080995e614f766bba216865f2543db6b2823307
unknown <surajpoddar098@gmail.com>
2026-09-20 02:23:01 +0545

    Set password validation rules

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -37,6 +37,8 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters long'],
+      maxlength: [128, 'Password cannot exceed 128 characters'],
     },
     phone: {
       type: String,
