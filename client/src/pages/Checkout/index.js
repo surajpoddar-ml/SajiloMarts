46644ca47de6ffb7c7eb27599d435db6b25f694a
unknown <surajpoddar098@gmail.com>
2026-09-25 22:59:15 +0545

    Complete the SajiloMarts quote payment and checkout experience

--- client/src/pages/Checkout/index.js
+++ client/src/pages/Checkout/index.js
@@ -1,2 +1,14 @@
 export { CheckoutPage } from './CheckoutPage.jsx';
+export { AuthoritativeQuoteReview } from './AuthoritativeQuoteReview.jsx';
+export { PaymentMethodSelector } from './PaymentMethodSelector.jsx';
+export { PaymentQrPresenter } from './PaymentQrPresenter.jsx';
+export { PaymentInstructionNotice } from './PaymentInstructionNotice.jsx';
+export { TransactionCodeInput } from './TransactionCodeInput.jsx';
+export { PaymentProofUploader } from './PaymentProofUploader.jsx';
+export { AddressSelector } from './AddressSelector.jsx';
+export { DeliveryAddressReview } from './DeliveryAddressReview.jsx';
+export { PaymentSummaryCard } from './PaymentSummaryCard.jsx';
+export { PaymentConfirmationCard } from './PaymentConfirmationCard.jsx';
+export { CodModeInfoCard } from './CodModeInfoCard.jsx';
+
 export default CheckoutPage;
